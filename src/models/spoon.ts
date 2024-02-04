4924f86a7e1801370076e2de5e4816baa6838f9d
Carlos Frias <carlos.a.frias@gmail.com>
2024-02-04 17:05:36 -0300

    chore: add spoon texture

--- src/models/spoon.ts
+++ src/models/spoon.ts
@@ -1,4 +1,10 @@
-import { Scene, SceneLoader, Vector3 } from "@babylonjs/core";
+import {
+  Color3,
+  PBRMetallicRoughnessMaterial,
+  Scene,
+  SceneLoader,
+  Vector3,
+} from "@babylonjs/core";
 
 export class Spoon {
   async render(scene: Scene) {
@@ -8,9 +14,19 @@ export class Spoon {
       "spoon.obj",
       scene
     );
-    const spoon = result.meshes[0];
-    spoon.position.z = 3;
+
+    result.meshes.forEach((mesh) => {
+      mesh.position.z = 2;
+    });
+
+    const spoon = result.meshes[1];
 
     spoon.rotate(new Vector3(0, 1, 0), Math.PI / 2);
+
+    const pbr = new PBRMetallicRoughnessMaterial("", scene);
+    pbr.baseColor = new Color3(0.55, 0.55, 0.55);
+    pbr.metallic = 2;
+    pbr.roughness = 0.6;
+    spoon.material = pbr;
   }
 }
