32b6c67f5c088ff5684efb6fb31243911fac0c9d
Carlos Frias <carlos.a.frias@gmail.com>
2024-02-04 12:03:21 -0300

    chore: add bag texture

--- src/models/bag.ts
+++ src/models/bag.ts
@@ -1,8 +1,8 @@
 import {
-  Color3,
   Scene,
   SceneLoader,
   StandardMaterial,
+  Texture,
   Vector3,
 } from "@babylonjs/core";
 
@@ -18,15 +18,28 @@ export class Bag {
     const mesh = result.meshes[0];
 
     const bagMaterial = new StandardMaterial("", scene);
-    bagMaterial.diffuseColor = Color3.FromHexString("#6B6055");
+    bagMaterial.diffuseTexture = new Texture(
+      "/models/bag/textures/kava_low_diffuse.png"
+    );
+    bagMaterial.emissiveTexture = new Texture(
+      "/models/bag/textures/kava_low_normal.png"
+    );
+    bagMaterial.ambientTexture = new Texture(
+      "/models/bag/textures/kava_low_ao.png"
+    );
+    bagMaterial.specularTexture = new Texture(
+      "/models/bag/textures/kava_low_specular.png"
+    );
+
     mesh.material = bagMaterial;
 
-    mesh.scaling.y = 0.25;
-    mesh.scaling.x = 0.25;
-    mesh.scaling.z = 0.25;
+    mesh.scaling.y = -0.25;
+    mesh.scaling.x = -0.25;
+    mesh.scaling.z = -0.25;
 
     mesh.position.z = 8;
 
-    mesh.rotate(new Vector3(1, 0, 0), -Math.PI / 2);
+    mesh.rotate(new Vector3(1, 0, 0), Math.PI / 2);
+    mesh.rotate(new Vector3(0, 0, 1), Math.PI);
   }
 }
